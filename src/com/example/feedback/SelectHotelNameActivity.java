ffefa0f4181dd585e12d918741903e62ed090a63
rajan talwar <rajan.talwar@oyorooms.com>
2014-10-11 22:48:39 +0530

    1.adding feedback activity which will be loaded after booking id is entered 2.This app would be generic one i.e usable by  both PROPERTY MANAGER aswell as by OYOS TEAM . 3. during login after asking the person to enter his identity if it comes out to be a oyo team person then showing a screen with list of hotels else moving directly to feedbackscreen

---
+++ src/com/example/feedback/SelectHotelNameActivity.java
@@ -0,0 +1,15 @@
+package com.example.feedback;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectHotelNameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_hotel_name);
+	}
+
+
+}
