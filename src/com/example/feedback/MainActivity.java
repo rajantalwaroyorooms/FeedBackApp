ee417385b9e722f53c37987e2e9a7259e2a275ef
rajan talwar <rajan.talwar@oyorooms.com>
2014-10-11 17:30:48 +0530

    adding the logic for handling user interactions during signin

--- src/com/example/feedback/MainActivity.java
+++ src/com/example/feedback/MainActivity.java
@@ -5,15 +5,18 @@ import org.json.JSONObject;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity implements RatingBar.OnRatingBarChangeListener {
 	private RatingBar roadDirections, cleanlinessAndHygiene,
@@ -23,6 +26,8 @@ public class MainActivity extends ActionBarActivity implements RatingBar.OnRatin
 	private EditText etComments;
 	private String roadDirectionsRating, cleanlinessAndHygieneRating, inRoomServicesRating,
 	            staffAttitudeRating, foodServedRatings, overAllExperienceRatings;
+	
+	public static final int SIGNATURE_ACTIVITY = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -162,5 +167,28 @@ public class MainActivity extends ActionBarActivity implements RatingBar.OnRatin
     	AlertDialog dialog = builder.create();
     	dialog.show();
     	
+    }
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch(requestCode) {
+        case SIGNATURE_ACTIVITY: 
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                String status  = bundle.getString("status");
+                if(status.equalsIgnoreCase("done")){
+                    Toast toast = Toast.makeText(this, "Signature capture successful!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.TOP, 105, 50);
+                    toast.show();
+                }
+            }
+            break;
+        }
+ 
+    }  
+    
+    public void proceedToCompleteClicked(View view){
+    	  Intent intent = new Intent(MainActivity.this, Capture.class); 
+          startActivityForResult(intent,SIGNATURE_ACTIVITY);
     }
 }
